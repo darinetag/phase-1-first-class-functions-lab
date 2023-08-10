17476d2c9c00a16c6f9c979e012d2d4aa6a76d1c
darinetag <tagdarinefatma@gmail.com>
2023-08-10 01:34:40 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);   
+}
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+const returnLastTwoDrivers = function(drivers2) {
+    return drivers2.slice(-2);   
+  }
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function(driversArray, driverSelectorFunction) {
+    return driverSelectorFunction(driversArray);
+  };
+  
+  console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers)); 
+  console.log(fareDoubler(10)); 
+  console.log(fareTripler(10)); 
+  
